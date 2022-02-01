e72c6d64cecc759ad8a22513301d48fa82cb04d8
linux_china <libing.chen@gmail.com>
2022-01-31 17:36:55 -0800

    Add more info

--- dubbo-demo/src/GreetingsService.java
+++ dubbo-demo/src/GreetingsService.java
@@ -1,3 +1,43 @@
+import java.util.List;
+import java.util.Map;
+
 public interface GreetingsService {
     String sayHi(String name);
+
+    String findNick(Integer id);
+
+    List<Map<String, String>> statistics();
+
+    User info(String name);
+
+    String exceptionCall(String name) throws Exception;
+
+    class User {
+        private int id;
+        private String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public User() {
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
