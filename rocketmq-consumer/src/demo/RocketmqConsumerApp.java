40719059430836e3c1fd84487d10932dfe32f608
linux_china <libing.chen@gmail.com>
2022-05-11 13:20:37 -0700

    build: update dependencies

--- rocketmq-consumer/src/demo/RocketmqConsumerApp.java
+++ rocketmq-consumer/src/demo/RocketmqConsumerApp.java
@@ -1,8 +1,8 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 8
-//DEPS org.springframework.boot:spring-boot-dependencies:2.6.6@pom
-//DEPS org.springframework.cloud:spring-cloud-dependencies:2021.0.1@pom
-//DEPS com.alibaba.cloud:spring-cloud-alibaba-dependencies:2021.0.1.0@pom
+//DEPS org.springframework.boot:spring-boot-dependencies:2.6.7@pom
+//DEPS org.springframework.cloud:spring-cloud-dependencies:2021.0.2@pom
+//DEPS com.alibaba.cloud:spring-cloud-alibaba-dependencies:2021.1@pom
 //DEPS org.springframework.boot:spring-boot-starter-webflux
 //DEPS com.alibaba.cloud:spring-cloud-starter-stream-rocketmq
 //FILES ../../resources/application.properties
